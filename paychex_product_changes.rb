aecb136100628595ec7ebd29928d1f62ffce19e0
Scott Young <scott.young@getbridge.com>
2021-08-24 10:26:42 -0600

    working file

--- paychex_product_changes.rb
+++ paychex_product_changes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-date = Date.parse('nov 2020')
-date_range = date.beginning_of_quarter..date.end_of_quarter
+date = Date.parse('jan 2021')
 
 def get_quarter(date)
   (date.month / 3.0).ceil
@@ -45,79 +44,100 @@ def worker_headers
    'Paychex Worker Full Name']
 end
 
-all_workers = []
+def get_account_data(date, record)
+
+  account = Account.with_deleted.find(record.account_id)
+
+  codes = record.object_changes.split(' ').grep(/LMS/)
+  created_date = record.created_at
+  created_quarter = get_quarter(created_date)
+  product_change_date = record.versions_created_at
+  product_change_quarter = get_quarter(product_change_date)
+
+
+  # if codes.first == 'LMS_ESS' && codes.last == 'LMS_ENH'
+  #   workers = account.workers.with_deleted.where.not('deleted_at < ?', product_change_date).where.not(
+  #     'created_at > ?', product_change_date.end_of_quarter
+  #   )
+  # end
+
+  # if codes.first == 'LMS_ENH' && codes.last == 'LMS_ESS'
+  #   workers = account.workers.with_deleted.where.not('created_at > ?', product_change_date).where.not(
+  #     'created_at > ?', product_change_date.end_of_quarter
+  #   )
+  # end
+  
+
+  workers = account.workers.with_deleted.where('created_at < ?', date.end_of_quarter).where.not('deleted_at < ?', date.beginning_of_quarter)
+
+  @all_workers << workers
+  [
+    'paychex',
+    record.client_type,
+    record.display_id,
+    record.account_id,
+    record.legal_name,
+    "https://#{record.bridge_subdomain}-paychex.bridgeapp.com",
+    created_date,
+    record.deleted_at,
+    'TRUE',
+    codes.first,
+    codes.last,
+    product_change_date,
+    product_change_date.year,
+    "Q#{product_change_quarter}",
+    workers.count,
+    created_date.year,
+    "Q#{created_quarter}"
+  ]
+end
+
+@all_workers = []
 
 CSV.open(open(account_file), 'w') do |csv|
   csv << account_headers
 
-  last_record = ''
-  found_ids = []
+  found_records = []
+  found_account_ids = []
 
-  PaperTrail::Version.where(item_type: 'PaychexAccount', created_at: date_range).where('object_changes ILIKE ?',
-                                                                                       '%product_code%').order(:item_id).find_each do |version|
-    account = PaychexAccount.with_deleted.find_by(id: version.item_id)
+  PaychexAccount.with_deleted.where(
+    'paychex_accounts.created_at < ?', date.end_of_quarter
+  ).joins(
+    'INNER JOIN versions ON versions.item_id = paychex_accounts.id'
+  ).where(
+    'versions.object_changes LIKE ?', '%product_code%'
+  ).select(
+    '*,
+     versions.created_at AS versions_created_at,
+     paychex_accounts.id AS p_account_id
+    '
+  ).order("versions.created_at DESC").each do |record|
+    next if record.blank?
+    next if found_records.include? record.p_account_id
+    next if found_account_ids.include? record.account_id
+
+    found_records << record.p_account_id
+    found_account_ids << record.account_id
+
+    next if record.versions_created_at > date.end_of_quarter
+
+    data = get_account_data(date, record)
+    csv << data
+  end
+ 
+  PaychexAccount.with_deleted.where.not(
+    id: found_records
+  ).where(
+    product_code: 'LMS_ENH'
+  ).where(
+    'created_at < ?', date.end_of_quarter
+  ).where.not('deleted_at > ?', date.end_of_quarter).find_each do |account|
 
     next if account.blank?
 
-    found_ids << account.id
-    codes = version.object_changes.split(' ').grep(/LMS/)
+    workers = account.workers.with_deleted.where('created_at < ?', date.end_of_quarter).where.not('deleted_at < ?', date.beginning_of_quarter)
 
-    data = {
-      account_id: account.id,
-      prior_code: codes.first,
-      new_code: codes.last
-    }
-
-    next if data == last_record
-
-    last_record = data
-    created_date = account.created_at
-    created_quarter = get_quarter(created_date)
-    product_change_date = version.created_at
-    product_change_quarter = get_quarter(product_change_date)
-
-    if codes.first == 'LMS_ESS' && codes.last == 'LMS_ENH'
-      workers = account.workers.with_deleted.where.not('deleted_at < ?', product_change_date).where.not(
-        'created_at > ?', product_change_date.end_of_quarter
-      )
-    end
-
-    if codes.first == 'LMS_ENH' && codes.last == 'LMS_ESS'
-      workers = account.workers.with_deleted.where.not('created_at > ?', product_change_date).where.not(
-        'created_at > ?', product_change_date.end_of_quarter
-      )
-    end
-
-    all_workers << workers
-
-    csv << [
-      'paychex',
-      account.client_type,
-      account.display_id,
-      account.account_id,
-      account.legal_name,
-      "https://#{account.bridge_subdomain}-paychex.bridgeapp.com",
-      created_date,
-      account.deleted_at,
-      'TRUE',
-      codes.first,
-      codes.last,
-      product_change_date,
-      product_change_date.year,
-      "Q#{product_change_quarter}",
-      workers.count,
-      created_date.year,
-      "Q#{created_quarter}"
-    ]
-  end
-
-  PaychexAccount.with_deleted.where.not(id: found_ids).where(product_code: 'LMS_ENH',
-                                                             created_at: date_range).find_each do |account|
-    date = account.created_at
-
-    workers = account.workers.with_deleted.where.not('created_at > ?', date.end_of_quarter)
-
-    all_workers << workers
+    @all_workers << workers
 
     quarter = get_quarter(date)
     csv << [
@@ -127,7 +147,7 @@ CSV.open(open(account_file), 'w') do |csv|
       account.account_id,
       account.legal_name,
       "https://#{account.bridge_subdomain}-paychex.bridgeapp.com",
-      date,
+      account.created_at,
       account.deleted_at,
       'FALSE',
       nil,
@@ -144,7 +164,7 @@ end
 
 CSV.open(open(worker_file), 'w') do |csv|
   csv << worker_headers
-  all_workers.flatten.each do |worker|
+  @all_workers.flatten.each do |worker|
     csv << [
       'paychex',
       worker.paychex_account.client_type,
