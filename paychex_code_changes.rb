aba25469d8490a6ce14a7968a5a3b40c52b8efb4
Scott Young <scott.young@getbridge.com>
2021-09-09 10:38:33 -0600

    adjust filters

--- paychex_code_changes.rb
+++ paychex_code_changes.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-date = Date.parse('may 2021')
+date = Date.parse('apr 2021')
 
 def get_quarter(date)
   (date.month / 3.0).ceil
 end
 
-account_file = "./paychex_enhanced_accounts_Q#{get_quarter(date)}_#{date.year}.csv"
+account_file = "./accounts_Q#{get_quarter(date)}_#{date.year}.csv"
 
 `touch #{account_file}`
 
@@ -31,30 +31,69 @@ def account_headers
 end
 
 CSV.open(open(account_file), 'w') do |csv|
-  ids = []
   csv << account_headers
 
+  @all_workers = []
+
   PaperTrail::Version.where(
-    item_type: 'PaychexAccount'
-  ).where.not(
-    'created_at > ?', date.end_of_quarter
-  ).where(
-    'object_changes LIKE ?', '%product_code%'
+    "object_changes LIKE '%product_code%'
+    AND created_at < ?", date.end_of_quarter
   ).select(
-    :item_id, :object_changes, 'MAX(created_at) AS created_at'
-  ).group(:item_id, :object_changes).each do |record|
-    ids << record.item_id
-    account = PaychexAccount.with_deleted.where(id: record.item_id).first
+    'DISTINCT ON ("item_id") *'
+  ).order(:item_id, created_at: :desc).each do |record|
+    account = PaychexAccount.with_deleted.where(
+      id: record.item_id
+    ).where(
+      'created_at < ?', date.end_of_quarter
+    ).where.not(
+      "legal_name ILIKE '%&&%'"
+    ).first
     next if account.blank?
 
     codes = record.object_changes.split(' ').grep(/LMS/)
+
+    next if codes.last == 'LMS_ESS' && !record.created_at.between?(date.beginning_of_quarter, date.end_of_quarter)
+
     account_created_date = account.created_at
     account_created_quarter = get_quarter(account_created_date)
     product_change_date = record.created_at
     product_change_quarter = get_quarter(product_change_date)
 
-    workers = account.workers.with_deleted.where('created_at < ?', date.end_of_quarter).where.not('deleted_at < ?',
-                                                                                                  date.beginning_of_quarter).count
+    if codes.first == 'LMS_ESS' && codes.last == 'LMS_ENH'
+      highest_date = [product_change_date, date.beginning_of_quarter].max
+
+      workers = account.workers.with_deleted.where(
+        'deleted_at > ?
+         OR deleted_at IS NULL
+        ', highest_date
+      ).where(
+        'created_at < ?', date.end_of_quarter
+      ).where.not(
+        "workers.composite_uid ILIKE '%duplicate%'"
+      )
+    elsif codes.first == 'LMS_ENH' && codes.last == 'LMS_ESS'
+      workers = account.workers.with_deleted.where(
+        'created_at < ?', product_change_date
+      ).where(
+        'deleted_at > ?
+         OR deleted_at IS NULL
+        ', date.beginning_of_quarter
+      ).where.not(
+        "workers.composite_uid ILIKE '%duplicate%'"
+      )
+    else
+      workers = account.workers.with_deleted.where(
+        'created_at < ?', date.end_of_quarter
+      ).where(
+        'deleted_at > ?
+         OR deleted_at IS NULL
+        ', date.beginning_of_quarter
+      ).where.not(
+        "workers.composite_uid ILIKE '%duplicate%'"
+      )
+    end
+
+    @all_workers << workers
 
     csv << [
       'paychex',
@@ -71,22 +110,36 @@ CSV.open(open(account_file), 'w') do |csv|
       product_change_date,
       product_change_date.year,
       "Q#{product_change_quarter}",
-      workers,
+      workers.count,
       account_created_date.year,
       "Q#{account_created_quarter}"
     ]
   end
 
-  PaychexAccount.with_deleted.where.not(
-    id: ids
+  pt_ids = PaperTrail::Version.where(item_type: 'PaychexAccount').where("object_changes LIKE '%product_code%'").pluck(:item_id).uniq
+  account_ids = PaychexAccount.with_deleted.where.not(id: pt_ids).where(product_code: 'LMS_ENH').pluck(:id).uniq
+
+  PaychexAccount.with_deleted.where(
+    id: account_ids
   ).where(
     product_code: 'LMS_ENH'
   ).where(
     'created_at < ?', date.end_of_quarter
   ).find_each do |account|
-    workers = account.workers.with_deleted.where('created_at < ?', date.end_of_quarter).where.not('deleted_at < ?',
-                                                                                                  date.beginning_of_quarter).count
-    quarter = get_quarter(date)
+    workers = account.workers.with_deleted.where(
+      'created_at < ?', date.end_of_quarter
+    ).where(
+      'deleted_at > ?
+       OR deleted_at IS NULL
+      ', date.beginning_of_quarter
+    ).where.not(
+      "workers.composite_uid ILIKE '%duplicate%'"
+    )
+
+    @all_workers << workers
+
+    quarter = get_quarter(account.created_at)
+
     csv << [
       'paychex',
       account.client_type,
@@ -102,9 +155,39 @@ CSV.open(open(account_file), 'w') do |csv|
       nil,
       nil,
       nil,
-      workers,
-      date.year,
+      workers.count,
+      account.created_at.year,
       "Q#{quarter}"
+    ]
+  end
+end
+
+worker_file = "./workers_Q#{get_quarter(date)}_#{date.year}.csv"
+
+`touch #{worker_file}`
+
+CSV.open(open(worker_file), 'w') do |csv|
+  csv << ['Bridge Instance',
+          'Client Type',
+          'Account Id',
+          'Client Name',
+          'Paychex User Id',
+          'Bridge User Id',
+          'User Created At',
+          'User Deleted At',
+          'Paychex Worker Full Name']
+
+  @all_workers.flatten.each do |worker|
+    csv << [
+      'paychex',
+      worker.paychex_account.client_type,
+      worker.paychex_account.account_id,
+      worker.paychex_account.legal_name,
+      worker.user_id,
+      worker.bridge_user_id,
+      worker.created_at,
+      worker.deleted_at,
+      worker.full_name
     ]
   end
 end
